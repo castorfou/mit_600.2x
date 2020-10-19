e010245593035ade9e8d293d5894ea9a9dc16d10
castorfou <guillaume.ramelet@gmail.com>
2020-10-19 13:58:52 +0200

    end of lecture 3: brute force, greedy comparaison

--- lectures/asset-v1 MITx+6.00.2x+3T2020+type@asset+block@lecture3-segment2/lecture3Segment2.py
+++ lectures/asset-v1 MITx+6.00.2x+3T2020+type@asset+block@lecture3-segment2/lecture3Segment2.py
@@ -82,4 +82,33 @@ def buildCityGraph(graphType):
     g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
     return g
     
-print(buildCityGraph(Graph))
+#print(buildCityGraph(Graph))
+
+
+nodes = []
+nodes.append(Node("ABC")) # nodes[0]
+nodes.append(Node("ACB")) # nodes[1]
+nodes.append(Node("BAC")) # nodes[2]
+nodes.append(Node("BCA")) # nodes[3]
+nodes.append(Node("CAB")) # nodes[4]
+nodes.append(Node("CBA")) # nodes[5]
+
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+    
+# Write the code that adds the appropriate edges to the graph
+# in this box.
+g.addEdge(Edge(g.getNode('ABC'), g.getNode('BAC')))
+g.addEdge(Edge(g.getNode('BAC'), g.getNode('BCA')))
+g.addEdge(Edge(g.getNode('BCA'), g.getNode('CBA')))
+g.addEdge(Edge(g.getNode('CBA'), g.getNode('CAB')))
+g.addEdge(Edge(g.getNode('CAB'), g.getNode('ACB')))
+g.addEdge(Edge(g.getNode('ACB'), g.getNode('ABC')))
+
+
+
+
+
+print('here is the graph of students:', g)    
+    
